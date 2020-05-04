01737be1258d6b4a4b02bd1f90e706769e3ff576
pmouhali <p.m.272019@gmail.com>
2020-05-04 13:40:14 +0200

    les commentaires étaient inutiles

--- process_tools.c
+++ process_tools.c
@@ -2,13 +2,6 @@
 
 #include "minishell.h"
 
-/*
-**	NAME : isbuiltin()
-**
-**	ROLE : indique si le nom de binaire passé en paramètre est enfait un builtin
-**		   renvoie l'index du builtin dans le tableau de fonctions ou -1 
-*/
-
 char *builtin_names[] = {
 	"exit", "cd", "echo", "env", "export", "pwd", "unset", NULL
 };
@@ -24,24 +17,10 @@ int		isbuiltin(char *bname)
 	));
 }
 
-/*
-**	NAME : call_builtin_function()
-**
-**	ROLE : appel la fonction builtin correspondant a l'index, avec les arguments passés en paramètres
-*/
-
 int		call_builtin_function(int builtin_index, char *args[])
 {
 	return (g_builtin_functions[builtin_index](args));
 }
-
-/*
-**	NAME : isbinary()
-**
-**	ROLE : indique si le nom de binaire passé en paramètres est disponible sur
-**			sur la machine retourne son chemin absolu si c'est le cas null sinon
-**			la recherche est basé sur la variable d'environment PATH comme d'hab
-*/
 
 char	*isbinary(char *bname)
 {
